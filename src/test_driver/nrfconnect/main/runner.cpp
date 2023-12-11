02ffddc0e3bbc1e5c428fdcc7e3f21ef320242b8
Jakub Latusek <j.latusek@samsung.com>
2023-12-11 18:59:57 +0100

    Update configuration to make compilation work for zephyr platform

--- src/test_driver/nrfconnect/main/runner.cpp
+++ src/test_driver/nrfconnect/main/runner.cpp
@@ -29,7 +29,16 @@ using namespace ::chip::DeviceLayer;
 
 LOG_MODULE_REGISTER(runner, CONFIG_MATTER_LOG_LEVEL);
 
-extern "C" int main(void)
+extern "C" {
+void console_init(void) {}
+
+int console_putchar(const char * chr)
+{
+    LOG_RAW(chr);
+    return 0;
+}
+
+int main(void)
 {
     VerifyOrDie(settings_subsys_init() == 0);
 
@@ -38,4 +47,5 @@ extern "C" int main(void)
     LOG_INF("CHIP test status: %d", status);
 
     _exit(status);
+}
 }
